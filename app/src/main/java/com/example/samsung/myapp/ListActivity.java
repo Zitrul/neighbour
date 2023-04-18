baa30b1533a0fbe8303785d7cedbda949587fc51
root <ardutex@gmail.com>
2023-04-18 20:27:27 +0300

    commit 20:27

--- app/src/main/java/com/example/samsung/myapp/ListActivity.java
+++ app/src/main/java/com/example/samsung/myapp/ListActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.ScrollView;
 
+import com.example.samsung.myapp.adapter.JobsAdapter;
 import com.example.samsung.myapp.adapter.MyRecyclerViewAdapter;
 
 import java.util.ArrayList;
@@ -17,25 +18,48 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
         ArrayList<Integer> viewColors = new ArrayList<>();
         viewColors.add(R.drawable.flag_russia);
         viewColors.add(R.drawable.flag_russia);
         viewColors.add(R.drawable.flag_russia);
         viewColors.add(R.drawable.flag_russia);
         viewColors.add(R.drawable.flag_russia);
-
         ArrayList<String> animalNames = new ArrayList<>();
         animalNames.add("Horse");
         animalNames.add("Cow");
         animalNames.add("Camel");
         animalNames.add("Sheep");
         animalNames.add("Goat");
+
         RecyclerView recyclerView = findViewById(R.id.rvJobsNew);
         LinearLayoutManager horizontalLayoutManager
                 = new LinearLayoutManager(ListActivity.this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(horizontalLayoutManager);
         MyRecyclerViewAdapter adapter = new MyRecyclerViewAdapter(this, viewColors, animalNames);
-
         recyclerView.setAdapter(adapter);
+
+        ArrayList<Integer> viewJobs = new ArrayList<>();
+        viewJobs.add(R.drawable.flag_russia);
+        viewJobs.add(R.drawable.flag_russia);
+        viewJobs.add(R.drawable.flag_russia);
+        viewJobs.add(R.drawable.flag_russia);
+        viewJobs.add(R.drawable.flag_russia);
+        viewJobs.add(R.drawable.flag_russia);
+        ArrayList<String> nameJobs = new ArrayList<>();
+        nameJobs.add("Horse");
+        nameJobs.add("Cow");
+        nameJobs.add("Camel");
+        nameJobs.add("Horse");
+        nameJobs.add("Cow");
+        nameJobs.add("Camel");
+
+
+        RecyclerView recViewJobs= findViewById(R.id.rvJobs);
+        LinearLayoutManager JobsManager
+                = new LinearLayoutManager(ListActivity.this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(JobsManager);
+        JobsAdapter adapterjobs = new JobsAdapter(this, viewJobs, nameJobs);
+        recyclerView.setAdapter(adapterjobs);
     }
 }
--- app/src/main/java/com/example/samsung/myapp/adapter/JobsAdapter.java
+++ app/src/main/java/com/example/samsung/myapp/adapter/JobsAdapter.java
@@ -40,9 +40,17 @@ public class JobsAdapter extends RecyclerView.Adapter<JobsAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         int color = mViewImg.get(position);
-        String animal = mJobs.get(position);
-        holder.myView.setImageResource(color);
-        holder.myTextView.setText(animal);
+
+        System.out.println(mViewImg.size());
+        String text1 = mJobs.get(position);
+        String text2 = mJobs.get(position+1);
+        String text3 = mJobs.get(position+2);
+        holder.myimgres.setImageResource(R.drawable.flag_russia);
+        holder.myimgresof1.setImageResource(R.drawable.flag_russia);
+        holder.myimgresof2.setImageResource(R.drawable.flag_russia);
+        holder.myTextView.setText(text1);
+        holder.myTextView.setText(text2);
+        holder.myTextView.setText(text3);
     }
 
 
@@ -53,13 +61,21 @@ public class JobsAdapter extends RecyclerView.Adapter<JobsAdapter.ViewHolder> {
 
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        ImageView myView;
+        ImageView myimgres;
+        ImageView myimgresof1;
+        ImageView myimgresof2;
         TextView myTextView;
-
+        TextView myTextView1;
+        TextView myTextView2;
         ViewHolder(View itemView) {
             super(itemView);
-            myView = itemView.findViewById(R.id.imageres);
-            myTextView = itemView.findViewById(R.id.tvAnimalName);
+            myimgres = itemView.findViewById(R.id.imageres);
+            myimgresof1 = itemView.findViewById(R.id.imageres1);
+            myimgresof2 = itemView.findViewById(R.id.imageres2);
+
+            myTextView = itemView.findViewById(R.id.tvname1);
+            myTextView1 = itemView.findViewById(R.id.tvname2);
+            myTextView2 = itemView.findViewById(R.id.tvname3);
             itemView.setOnClickListener(this);
         }
 
